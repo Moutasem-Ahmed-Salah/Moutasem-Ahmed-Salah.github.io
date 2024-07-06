91ea06266b1c9ba4af729ed2e618f3116313bff7
Moutasem-Ahmed-Salah <moutasemahmed122@gmail.com>
2024-07-06 03:42:15 +0300

    Responsive Page

--- src/slider.js
+++ src/slider.js
@@ -1,16 +1,27 @@
-const swiper = new Swiper('.swiper-container', {
-    loop: true,
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 10,
     pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
+      el: '.swiper-pagination',
+      clickable: true,
     },
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     },
-    slidesPerView: 1,
-    slidesPerGroup: 1,
-});
-
-
-
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+      },
+    }
+  });
+  
